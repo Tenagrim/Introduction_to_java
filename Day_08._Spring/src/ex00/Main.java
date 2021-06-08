0b65adbf965f2d1a92294bde86509d851834f5d0
Tenagrim <tenagrim@gmail.com>
2021-06-08 21:39:43 +0300

    init day 08

---
+++ Day_08._Spring/src/ex00/Main.java
@@ -0,0 +1,12 @@
+package ex00;
+
+public class Main {
+    public static void main(String[] args) {
+        PreProcessor preProcessor = new PreProcessorToUpperImpl();
+        Renderer renderer = new RendererErrImpl(preProcessor);
+        PrinterWithPrefixImpl printer = new PrinterWithPrefixImpl(renderer);
+        printer.setPrefix("Prefix");
+        printer.print("Hello!");
+        
+    }
+}
